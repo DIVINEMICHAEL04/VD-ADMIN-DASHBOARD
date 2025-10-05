391e2079915199c6253f1b2537d0a32419f5bf1f
DIVINEMICHAEL04 <divinegodwinmichael@gmail.com>
2025-10-05 03:20:33 +0100

    Fix: corrected CSS import case sensitivity

--- src/pages/SubscriptionDashboard/SubscriptionDashboard.jsx
+++ src/pages/SubscriptionDashboard/SubscriptionDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import NavSection from "../../component/Overview-dashboard/nav-section/nav_section"
 import SubscriptionSection from "../../component/Users-subscription/Users_subscription"
 import { Outlet } from "react-router-dom";
-import "./SubscriptionDashBoard.css"
+import "./SubscriptionDashboard.css"
 
 export default function SubscriptionSect(){
     return(
